b59f8fc4c96fa69b2daf2a3efe20f298775578a2
infernostars <blazem7708.2@gmail.com>
2022-06-26 14:57:27 -0600

    add render

--- mcwebsite/settings.py
+++ mcwebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://mcrenderer.onrender.com/"]
+ALLOWED_HOSTS = ["mcrenderer.onrender.com"]
 
 
 # Application definition
