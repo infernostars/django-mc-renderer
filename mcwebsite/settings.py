35ce126d53369d9a4f36df76b55884b0f1501e35
infernostars <blazem7708.2@gmail.com>
2022-06-26 13:55:06 -0600

    Add the allowed host

--- mcwebsite/settings.py
+++ mcwebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_993vpc3nf-1kf1rx!my-t&3q7%dcx+q7*mi_*0r2x6ex^cwi3'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://mcrenderer.onrender.com/"]
 
 
 # Application definition
