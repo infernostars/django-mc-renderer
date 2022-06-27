256fe0d749cf6f057a842a005ab324da7de91714
infernostars <blazem7708.2@gmail.com>
2022-06-27 00:51:45 -0600

    i request to use requests

--- display/views.py
+++ display/views.py
@@ -1,3 +1,4 @@
+import imp
 from django.shortcuts import render
 
 # Create your views here.
@@ -5,8 +6,26 @@ from django.http import HttpResponse
 
 from PIL import Image, ImageDraw, ImageFont
 
+import requests
+
 
 def generator(request):
+    qd = request.GET
+    x = min(2048, int(qd.get("x", 250)))
+    y = min(2048, int(qd.get("y", 250)))
+    color = qd.get("color", "192,192,192")
+    color = tuple(map(int, color.split(',')))
+    text = str(qd.get("text", f"{x} × {y}"))
+    image = Image.new('RGB', (x, y), color)
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('display/fonts/SourceSansPro-Regular.ttf', size=24)
+    draw.text((x/2, y/2), text, font=font, anchor="mm", fill=tuple(round(cr/2) for cr in color))
+    response = HttpResponse(content_type="image/png")
+    image.save(response, "PNG")
+    return response
+
+
+def avatar(request):
     qd = request.GET
     x = min(2048, int(qd.get("x", 250)))
     y = min(2048, int(qd.get("y", 250)))
