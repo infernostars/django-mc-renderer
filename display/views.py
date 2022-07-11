92cb4f6bd4a41b54bcb2e2666f5d49e4645ccd12
infernostars <blazem7708.2@gmail.com>
2022-07-11 15:14:41 -0600

    he he he haw

--- display/views.py
+++ display/views.py
@@ -1,12 +1,8 @@
-import imp
-from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
 
 from PIL import Image, ImageDraw, ImageFont
 
-import requests
+import requests, psycopg2
 
 
 def generator(request):
@@ -18,24 +14,8 @@ def generator(request):
     text = str(qd.get("text", f"{x} × {y}"))
     image = Image.new('RGB', (x, y), color)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('display/fonts/SourceSansPro-Regular.ttf', size=24)
-    draw.text((x/2, y/2), text, font=font, anchor="mm", fill=tuple(round(cr/2) for cr in color))
-    response = HttpResponse(content_type="image/png")
-    image.save(response, "PNG")
-    return response
-
-
-def avatar(request):
-    qd = request.GET
-    x = min(2048, int(qd.get("x", 250)))
-    y = min(2048, int(qd.get("y", 250)))
-    color = qd.get("color", "192,192,192")
-    color = tuple(map(int, color.split(',')))
-    text = str(qd.get("text", f"{x} × {y}"))
-    image = Image.new('RGB', (x, y), color)
-    draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('display/fonts/SourceSansPro-Regular.ttf', size=24)
-    draw.text((x/2, y/2), text, font=font, anchor="mm", fill=tuple(round(cr/2) for cr in color))
+    font = ImageFont.truetype('display/fonts/SourceSansPro-Regular.ttf', size=min(120, max(12, int(qd.get("fontsize", 24)))))
+    draw.multiline_text((x/2, y/2), text, font=font, anchor="mm", fill=tuple(round(cr/2) for cr in color))
     response = HttpResponse(content_type="image/png")
     image.save(response, "PNG")
     return response
