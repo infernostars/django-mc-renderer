72160683f58f31a7557f005f5cc85a8bf915413d
infernostars <blazem7708.2@gmail.com>
2022-07-27 17:51:10 -0600

    code style

--- display/views.py
+++ display/views.py
@@ -12,9 +12,13 @@ def generator(request):
     color = qd.get("color", "192,192,192")
     color = tuple(map(int, color.split(',')))
     text = str(qd.get("text", f"{x} × {y}"))
+    font = str(qd.get("font", "serif"))
     image = Image.new('RGB', (x, y), color)
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype('display/fonts/monospace/SourceSansPro-Regular.ttf', size=min(120, max(12, int(qd.get("fontsize", 24)))))
+    try:
+        font = ImageFont.truetype(f'display/fonts/{font}/Regular.ttf', size=min(120, max(12, int(qd.get("fontsize", 24)))))
+    except:
+        font = ImageFont.truetype('display/fonts/serif/Regular.ttf', size=min(120, max(12, int(qd.get("fontsize", 24)))))
     draw.multiline_text((x/2, y/2), text, font=font, anchor="mm", fill=tuple(round(cr/2) for cr in color))
     response = HttpResponse(content_type="image/png")
     image.save(response, "PNG")
