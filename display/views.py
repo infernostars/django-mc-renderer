680091555772ce59d298dc8549ffc944a8eb60c4
infernostars <blazem7708.2@gmail.com>
2022-08-20 15:01:23 -0600

    testing place

--- display/views.py
+++ display/views.py
@@ -4,6 +4,10 @@ from PIL import Image, ImageDraw, ImageFont
 
 import requests, psycopg2
 
+def unpack(string):
+    s = string
+    s.replace("7", "66").replace("6", "55").replace("5", "44").replace("4", "33").replace("3", "22").replace("2", "11").replace("1", "00").replace("0", "dd")
+    return s
 
 def generator(request):
     qd = request.GET
@@ -23,3 +27,32 @@ def generator(request):
     response = HttpResponse(content_type="image/png")
     image.save(response, "PNG")
     return response
+
+def place(request):
+    image = Image.new('RGB', (64, 64), 0xE6E7E8)
+    qd = request.GET
+    encodedinfo = unpack(qd.get('info', "7777777777777777"))
+    encodedlist = list(encodedinfo)
+    colorlist = []
+    convdict = {
+        "a": 0x31373D,
+        "b": 0x585954,
+        "c": 0xB3B1AB,
+        "d": 0xE6E7E8,
+        "e": 0xFF83F8,
+        "f": 0xF63E06,
+        "g": 0xF4900C,
+        "h": 0xC1694F,
+        "i": 0xFDCB58,
+        "j": 0xBCDB38,
+        "k": 0x78B159,
+        "l": 0x0DDBD0,
+        "m": 0x55ACEE,
+        "n": 0x0C2ED2,
+        "o": 0xAA8ED6,
+        "p": 0x801D6C
+    }
+    for point in encodedlist:
+        colorlist.append(convdict["point"])
+    return str(colorlist)
+    
